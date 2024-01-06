6863154039188c5251aba812eb9150abceab7214
LeeYang <leeyang031110@gmail.com>
2024-01-06 18:27:49 +0800

    save

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -1,21 +1,76 @@
 #include "phonebook.hpp"
 
+Phonebook::Phonebook()
+{
+	Phonebook::index = 0;
+}
+
 void Phonebook::add_contacts()
 {
-	std::string fn;
+	std::string input;
 	while (true)
 	{
 		std::cout << "Enter first name: " << std::endl;
-		std::getline(std::cin, fn);
-		if (fn.empty())
+		std::getline(std::cin, input);
+		if (input.empty())
 			std::cout << "No one has an empty name! Try again, and be honest." << std::endl;
 		else
 		{
-			std::cout << "first name: " << fn << std::endl;
+			Phonebook::contacts[Phonebook::index].set_first_name(input);
+			break;
+		}
+	}
+	while (true)
+	{
+		std::cout << "Enter last name: " << std::endl;
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "No one has an empty name! Try again, and be honest." << std::endl;
+		else
+		{
+			Phonebook::contacts[Phonebook::index].set_last_name(input);
+			break;
+		}
+	}
+	while (true)
+	{
+		std::cout << "Enter nickname: " << std::endl;
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "No one has an empty name! Try again, and be honest." << std::endl;
+		else
+		{
+			Phonebook::contacts[Phonebook::index].set_nickname(input);
+			break;
+		}
+	}
+	while (true)
+	{
+		std::cout << "Enter phone number: " << std::endl;
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "No one has an empty phone number! Try again, and be honest." << std::endl;
+		else
+		{
+			Phonebook::contacts[Phonebook::index].set_phone_number(input);
+			break;
+		}
+	}
+	while (true)
+	{
+		std::cout << "Enter darkest secret: " << std::endl;
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "No one has an empty secret! Try again, and be honest." << std::endl;
+		else
+		{
+			Phonebook::contacts[Phonebook::index].set_darkest_secret(input);
 			break;
 		}
 	}
 	Phonebook::index++;
+	if (Phonebook::index == 8)
+		Phonebook::index = 0;
 	std::cout << "loop ended" << std::endl;
 }
 
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -2,8 +2,11 @@
 
 class Phonebook{
 public:
+	Phonebook();
 	void add_contacts();
+	void search_and_print_contact(std::string);
 	Contact search_contact(std::string);
+	void print_contact(Contact);
 private:
 	Contact contacts[8];
 	int index;
