afe8bce132efdb5981ddf183edfbf27848bd4efe
MysteriousBoner <106369958+MysteriousLoner@users.noreply.github.com>
2024-01-18 00:38:55 +0000

    done

---
+++ cpp04/ex01/main.cpp
@@ -0,0 +1,26 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+int main()
+{
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	delete j;
+	delete i;
+
+	Animal* animals[100];
+
+	for (int i = 0; i < 50; i++)
+		animals[i] = new Dog();
+
+	for (int i = 50; i < 100; i++)
+		animals[i] = new Cat();
+
+	for (int i = 0; i < 100; i++)
+		delete animals[i];
+
+	return 0;
+}
